7db2514edc3a04a7acd5fa0ae025b0944d111518
farbs03 <cgeraldfarber@icloud.com>
2021-02-08 09:19:56 -0500

    minor changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,32 @@
-import {Box, Text, ChakraProvider, Flex, Button} from "@chakra-ui/react"
+import {Box, Text, ChakraProvider, Flex, Button, Container, Center, Image, Stack} from "@chakra-ui/react"
 import {CheckIcon} from "@chakra-ui/icons"
 
 import SideNav from "./SideNav"
+
+import logo from "../logo2.png"
 
 
 function Home() {
 
 
   return (
-    <div>home</div>
+    <Flex 
+        direction="column"
+        alignItems="center"
+        justifyContent="space-between"
+        w={1400}
+      >
+        <Stack direction="column">
+          <Center mt="50px">
+            <Text fontSize="30px" fontWeight="bold">Incentiva</Text>
+            <br></br>
+          </Center>
+          <Image
+                boxSize="130px"
+                src={logo}
+            />
+        </Stack>
+    </Flex>
   );
 }
 
