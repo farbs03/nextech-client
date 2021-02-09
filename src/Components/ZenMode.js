34edcfe3cd48e98a467f80006d30aa58ef5d3379
ArnavMeh <55332253+ArnavMeh@users.noreply.github.com>
2021-02-08 19:49:31 -0500

    diff Zen Mode

--- src/Components/ZenMode.js
+++ src/Components/ZenMode.js
@@ -2,11 +2,144 @@ import React, {useState, useEffect, useContext} from 'react'
 
 import userData from '../mockUserData.json';
 
-import { CircularProgress, CircularProgressLabel, IconButton, Container } from "@chakra-ui/react"
-import {StepForwardOutlined, RetweetOutlined, CheckOutlined, PauseOutlined, CaretRightOutlined, ArrowUpOutlined, ArrowDownOutlined} from '@ant-design/icons'
+import { CircularProgress, CircularProgressLabel, IconButton, Container, Input } from "@chakra-ui/react"
+import {StepForwardOutlined,StepBackwardOutlined, RetweetOutlined, CheckOutlined, PauseOutlined, CaretRightOutlined, ArrowUpOutlined, ArrowDownOutlined} from '@ant-design/icons'
+
 
 
 const ZenMode = () => {
+
+    const [user, setUser] = useState(userData)
+
+    const currDate = new Date()
+    const [t, setT] = useState(3600)
+    const [paused, setPaused] = useState(false)
+    const [active, setActive] = useState(true) //whether buttons are active
+    
+
+
+    useEffect(() => {
+        if (!paused) {
+            if (t!=0) {
+                setTimeout(() => {
+                    setT(t-1)
+                }, 1000)
+            }
+        }
+    }, [t, paused])
+
+    useEffect(() => {
+        if (paused) {
+            var min = Math.floor(parseInt(timeForm.substring(0, timeForm.indexOf(":"))))
+            var sec = Math.floor(parseInt(timeForm.substring(timeForm.indexOf(":")+1)))
+            if (0<=min && 0<=sec && sec<=60) {
+                setT(min*60 + sec)
+            }
+        } else {
+            setTimeForm(timeString(t))
+        }
+    }, [paused])
+
+
+
+    const timeString = (t) => {
+        var str = Math.floor(t/60) + ":" + t%60
+        if (t%60<10) {
+            str = str.replace(":", ":0")
+        }
+        if (Math.floor(t/60)<10) {
+            str = "0" + str
+        }
+        return str
+    }
+    const [timeForm, setTimeForm] = useState(timeString(t))
+
+
+
+    const addMin = (inc) => {
+        if (t+inc*60>5) {
+            if (paused) {
+                setT(t+inc*60)
+            } else {
+                setActive(false)
+                setPaused(true)
+                setTimeout(() => {
+                    setT(t+inc*60)
+                    setTimeout(() => {
+                        setActive(true)
+                        setPaused(false)
+                    }, 1000)
+                }, 1000)
+            }
+        }
+    }
+    const skipButton = (inc, size) => (
+        <IconButton
+            icon={<div style={{fontSize:"20px"}}>{inc>0? "+": ""}{inc}</div>}
+            onClick={() => {if (active) {addMin(inc)}}}
+            colorScheme="blue"
+            isRound style={active? {width: size, height: size} : {width: size, height: size, backgroundColor: "gray"}}
+        />
+    )
+       
+
+
+    return (
+        <div id="zen-body">
+
+            {/* Timer */}
+            <div style={{display:"flex", justifyContent:"center", alignItems:"center", margin:"20px 0 40px 0"}}>
+                <CircularProgress value={t/36} className="home-progress" size="400px" thickness="5px" color="purple.500">
+                    <CircularProgressLabel style={{display:'flex', justifyContent:"center", fontSize:"60px"}}>
+                        {paused?
+                            <Input 
+                                variant="unstyled"
+                                value={timeForm}
+                                style={{width:"142px", fontSize:"60px", height:"60px"}}
+                                onChange = {(e) => setTimeForm(e.target.value)}
+                                onKeyPress = {e => {
+                                    if (e.key == 'Enter') {
+                                        setPaused(false)
+                                    }
+                                }}
+                            />
+                        :
+                            <div
+                                style={{fontSize:"60px", height:"60px"}}
+                                onClick={() => {
+                                    setPaused(true)
+                                    setTimeForm(timeString(t))
+                                }}
+                            >{timeString(t)}</div>
+                        }
+                    </CircularProgressLabel>
+                </CircularProgress>
+            </div>
+            
+
+            <div style={{display:"flex", justifyContent:"space-evenly", alignItems:"center", margin:"20px 0 40px 0", width:"100%"}}>
+                {skipButton(-15, "60px")}
+                {skipButton(-5, "70px")}
+                <IconButton
+                    icon={paused?
+                        <CaretRightOutlined style={{fontSize:"30px", marginLeft:"4px", transform: "scaleX(1.3)"}}/>
+                    :
+                        <PauseOutlined style={{fontSize:"30px"}}/>
+                    }
+                    colorScheme="blue"
+                    isRound style={active? {width: "80px", height: "80px"} : {width: "80px", height: "80px", backgroundColor: "gray"}}
+                    onClick={() => setPaused(!paused)}
+                />
+                {skipButton(5, "70px")}
+                {skipButton(+15, "60px")}
+            </div>
+        </div>
+    )
+    
+}
+
+
+const ZenMode2 = () => {
     
     const currDate = new Date()
     
@@ -63,6 +196,67 @@ const ZenMode = () => {
         setCurrInd(nextInd)
     }
     
+    const TaskTypes = [{
+        title: "Current",
+        data: 0,
+        buttonicon: (index) => (
+            <>
+                <CheckOutlined style={{color: active? "#52c41a": "gray"}} onClick={() => {
+                    if (active) {
+                        updateStatus(index, 1)
+                        setCurrInd(-1)
+                    }
+                }}/>
+                &nbsp; &nbsp; &nbsp;
+                <ArrowDownOutlined style={{color: active? "black": "gray"}} onClick={() => {
+                    if (active) {
+                        updateStatus(index, -1)
+                        setCurrInd(-1)
+                    }
+                }}/>
+            </>
+        )
+    },{
+        title: "On Deck",
+        data: -1,
+        buttonicon: (index) => (
+            <>
+                <CheckOutlined style={{color: active? "#52c41a": "gray"}} onClick={() => {
+                    if (active) {
+                        updateStatus(index, 1)}
+                    }
+                }/>
+                &nbsp; &nbsp; &nbsp;
+                <CaretRightOutlined style={{color: active? "#3182ce": "gray", fontSize:"17px", width:"14px", margin:"0 2px 0 -1px", transform: "scaleX(1.4)"}} onClick={() => {
+                    if (active) {
+                        if (currInd != -1) {updateStatus(currInd, -1)}
+                        updateStatus(index, 0)
+                        setCurrInd(index)
+                    }
+                }}/>
+            </>
+        )
+    },{
+        title: "Completed",
+        data: 1,
+        buttonicon: (index) => (
+            <>
+                <ArrowUpOutlined style={{color: active? "black": "gray"}} onClick={() => {
+                    if (active) {
+                        updateStatus(index, -1)
+                    }
+                }}/>
+                &nbsp; &nbsp; &nbsp;
+                <CaretRightOutlined style={{color: active? "#3182ce": "gray", fontSize:"17px", width:"14px", margin:"0 2px 0 -1px", transform: "scaleX(1.4)"}} onClick={() => {
+                    if (active) {
+                        if (currInd != -1) {updateStatus(currInd, -1)}
+                        updateStatus(index, 0)
+                        setCurrInd(index)
+                    }
+                }}/>
+            </>
+        )
+    }]
 
 
     return (
@@ -108,67 +302,7 @@ const ZenMode = () => {
 
 
             {/* Change Activity */}
-            {[{
-                title: "Current",
-                data: 0,
-                buttonicon: (index) => (
-                    <>
-                        <CheckOutlined style={{color: active? "#52c41a": "gray"}} onClick={() => {
-                            if (active) {
-                                updateStatus(index, 1)
-                                setCurrInd(-1)
-                            }
-                        }}/>
-                        &nbsp; &nbsp; &nbsp;
-                        <ArrowDownOutlined style={{color: active? "black": "gray"}} onClick={() => {
-                            if (active) {
-                                updateStatus(index, -1)
-                                setCurrInd(-1)
-                            }
-                        }}/>
-                    </>
-                )
-            },{
-                title: "On Deck",
-                data: -1,
-                buttonicon: (index) => (
-                    <>
-                        <CheckOutlined style={{color: active? "#52c41a": "gray"}} onClick={() => {
-                            if (active) {
-                                updateStatus(index, 1)}
-                            }
-                        }/>
-                        &nbsp; &nbsp; &nbsp;
-                        <CaretRightOutlined style={{color: active? "#3182ce": "gray", fontSize:"17px", width:"14px", margin:"0 2px 0 -1px", transform: "scaleX(1.4)"}} onClick={() => {
-                            if (active) {
-                                if (currInd != -1) {updateStatus(currInd, -1)}
-                                updateStatus(index, 0)
-                                setCurrInd(index)
-                            }
-                        }}/>
-                    </>
-                )
-            },{
-                title: "Completed",
-                data: 1,
-                buttonicon: (index) => (
-                    <>
-                        <ArrowUpOutlined style={{color: active? "black": "gray"}} onClick={() => {
-                            if (active) {
-                                updateStatus(index, -1)
-                            }
-                        }}/>
-                        &nbsp; &nbsp; &nbsp;
-                        <CaretRightOutlined style={{color: active? "#3182ce": "gray", fontSize:"17px", width:"14px", margin:"0 2px 0 -1px", transform: "scaleX(1.4)"}} onClick={() => {
-                            if (active) {
-                                if (currInd != -1) {updateStatus(currInd, -1)}
-                                updateStatus(index, 0)
-                                setCurrInd(index)
-                            }
-                        }}/>
-                    </>
-                )
-            }].map(item => (
+            {TaskTypes.map(item => (
                 <div style={{minHeight:"60px"}}>
                     <div style={{marginLeft:"10px", marginTop:"30px", fontSize:"24px"}}>{item.title}</div>
                     <>{user.days[0].workTasks.map((task, index) => {
